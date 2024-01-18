f532ae80861ebbc4cc9d2ed5e84ea4483eca1dba
박종빈 <runnz121@gmail.com>
2024-01-18 23:29:19 +0900

    [feat] group anagrams

---
+++ src/main/java/algorithmKotlin/leetCode/GroupAnagrams.java
@@ -0,0 +1,22 @@
+package algorithmKotlin.leetCode;
+
+import java.util.List;
+
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        GroupAnagrams ga = new GroupAnagrams();
+        List<List<String>> res = ga.groupAnagrams(strs);
+        System.out.println(res);
+    }
+}
+
