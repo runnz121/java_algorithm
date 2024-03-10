eaedd8f294385d658839c3dbeda4f372b645e76e
박종빈 <runnz121@gmail.com>
2024-03-11 00:04:27 +0900

    [feat] 11811_bitmask scanncer 로 변경

--- src/main/java/algorithm/baekjoon/_11811_bitMask.java
+++ src/main/java/algorithm/baekjoon/_11811_bitMask.java
@@ -1,35 +1,24 @@
 package algorithm.baekjoon;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class _11811_bitMask {
 
-    static int N;
-    static int [] p;
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws  Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = scanner.nextInt();
+        int[] p = new int[N];
 
-        N = Integer.parseInt(st.nextToken());
-        p = new int[1001];
-
-        List<Integer> results = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            StringTokenizer st1 =  new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < N; j++) {
-                if (i == j) {
-                    continue;
-                }
-                int k = Integer.parseInt(st1.nextToken());
-                p[i] = p[i] | k;
+                int k = scanner.nextInt();
+                if (i == j) continue;
+                p[i] |= k;
             }
-            results.add(p[i]);
         }
-        results.forEach(it -> System.out.print(it + " "));
+        for (int i = 0; i < N; i++)
+            System.out.print(p[i] + " ");
+        scanner.close();
     }
 }
