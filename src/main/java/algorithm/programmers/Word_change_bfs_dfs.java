f5768b82eddf90aa92e47502ef146a7421775da3
박종빈 <runnz121@gmail.com>
2024-06-17 23:46:50 +0900

    [feat] 단어변환 dfs

--- src/main/java/algorithm/programmers/Word_change_bfs_dfs.java
+++ src/main/java/algorithm/programmers/Word_change_bfs_dfs.java
@@ -3,13 +3,42 @@ package algorithm.programmers;
 //https://school.programmers.co.kr/learn/courses/30/lessons/43163
 public class Word_change_bfs_dfs {
 
+    static boolean[] visited;
+    static int answer = 0;
+
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
+        visited = new boolean[words.length];
 
-
-
-        System.out.println(answer);
+        dfs(begin, target, words, 0);
         return answer;
+    }
+
+    public static void dfs(String begin, String target, String[] words, int cnt) {
+        // 같은 글자면 종료
+        if (begin.equals(target)) {
+            answer = cnt;
+            return;
+        }
+
+        // 이미 방문한 글자인 경우 패스
+        for (int i = 0; i < words.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            // 들어온 글자와 배열에 존재하고있는 방문하지 않은 글자 중 같은 글자 세기
+            int k = 0;
+            for (int j = 0; j < begin.length(); j++) {
+                if (begin.charAt(j) == words[i].charAt(j)) {
+                    k++;
+                }
+            }
+            // 한글자 빼고 모두 같은 경우
+            if (k == begin.length() - 1) {
+                visited[i] = true;
+                dfs(words[i], target, words, cnt + 1);
+                visited[i] = false;
+            }
+        }
     }
 
     public static void main(String[] args) {
