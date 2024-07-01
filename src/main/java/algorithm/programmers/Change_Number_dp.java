585c646c6373ada3fe4a5661f71afbe0ec096032
박종빈 <runnz121@gmail.com>
2024-07-01 22:01:43 +0900

    [feat] 숫자변환 dp

---
+++ src/main/java/algorithm/programmers/Change_Number_dp.java
@@ -0,0 +1,38 @@
+package algorithm.programmers;
+
+public class Change_Number_dp {
+
+    public int solution(int x, int y, int n) {
+        int answer = 0;
+        int[] dp = new int[y + 1];
+
+        if (x == y) {
+            return 0;
+        }
+
+        for (int i = x; i <= y; i++) {
+            if (i != x && dp[i] == 0) {
+                dp[i] = -1;
+                continue;
+            }
+
+            if (i + n <= y) {
+                dp[i + n] = (dp[i + n] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i + n]);
+            }
+            if(i * 2 <= y) {
+                dp[i * 2] = (dp[i * 2] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 2]);
+            }
+            if(i * 3 <= y) {
+                dp[i * 3] = (dp[i * 3] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 3]);
+            }
+        }
+
+        answer = dp[y];
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Change_Number_dp cnd = new Change_Number_dp();
+        cnd.solution(10, 40, 5);
+    }
+}
