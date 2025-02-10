45a0a1310efddfab630f55585c029bfa5dcbce2c
runnz121 <runnz121@gmail.com>
2025-02-10 23:53:27 +0900

    [feat] 2343 이분탐색 푸는 중

---
+++ src/main/java/algorithm/baekjoon/_2343_binary_search.java
@@ -0,0 +1,60 @@
+package algorithm.baekjoon;
+
+import java.util.*;
+import java.io.*;
+public class _2343_binary_search {
+
+    static int N;
+    static int M;
+    static List<Integer> lists = new ArrayList<>();
+    static int answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line1 = br.readLine().split(" ");
+
+        N = Integer.parseInt(line1[0]);
+        M = Integer.parseInt(line1[1]);
+
+        String[] line2 = br.readLine().split(" ");
+
+        int left = 0;
+        int right = 0;
+
+        for (String line : line2) {
+            Integer ele = Integer.parseInt(line);
+            left = Math.max(ele, left);
+            right += ele;
+            lists.add(ele);
+        }
+
+        while (left <= right) {
+
+            int total = 0;
+            int mid = left + right / 2;
+            int count = 1;
+
+            for (int i = 0; i < N; i++) {
+                total += lists.get(i);
+                if (total + lists.get(i) > mid) {
+                    total = 0;
+                    count ++ ;
+                }
+
+                total += lists.get(i);
+            }
+
+            if (total > 0) {
+                count += 1;
+            }
+
+            if (count < M) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(left);
+    }
+}
