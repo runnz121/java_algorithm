40853631031ff07b935486c84fd8452fd9f2df46
runnz121 <runnz121@gmail.com>
2025-02-13 00:31:09 +0900

    재귀 수정

--- src/main/java/algorithm/programmers/Babbling.java
+++ src/main/java/algorithm/programmers/Babbling.java
@@ -1,56 +1,44 @@
 package algorithm.programmers;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Babbling {
 
     static List<String> ava = Arrays.asList("aya", "ye", "woo", "ma");
     static Set<String> extra = new HashSet<>();
-    static boolean[] visited;
+    boolean[] visited = new boolean[4];
 
     public int solution(String[] babbling) {
         int answer = 0;
 
-        for (int i = 2 ; i <= 4; i++) {
-            visited = new boolean[ava.size()];
-            for (int k = 0; k < 4; k++) {
-                visited[k] = true;
-                recursive(i, k, ava.get(k));
-            }
-        }
-
-        List<String> extraLists = new ArrayList<>(extra.stream().toList());
-        extraLists.addAll(ava);
-        Set<String> collect = extraLists.stream().collect(Collectors.toSet());
+        recursive("", 0);
 
         for (int x = 0; x < babbling.length; x++) {
-            if (collect.contains(babbling[x])) answer += 1;
+            if (extra.contains(babbling[x])) answer += 1;
         }
 
-        System.out.println(answer);
-        System.out.println(collect);
         return answer;
     }
 
-    public void recursive(int targetDepth,
-                          int start,
-                          String word) {
+    public void recursive(String current,
+                          int depth) {
 
-        if (targetDepth <= start) {
-            extra.add(word);
+        if (depth > 0) {
+            extra.add(current);
+        }
+
+        if (depth == 4) {
             return;
         }
 
         for (int i = 0; i < ava.size(); i++) {
             if (visited[i] == false) {
                 visited[i] = true;
-                recursive(targetDepth, i + 1, word + ava.get(i));
+                recursive(current + ava.get(i), depth + 1);
                 visited[i] = false;
             }
         }
     }
-
 
     public static void main(String[] args) {
         Babbling bb = new Babbling();
