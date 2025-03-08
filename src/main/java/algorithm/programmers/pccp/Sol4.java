8ebd7795b64f5fde4a8858f11ab9eb2d8354d02c
runnz121 <runnz121@gmail.com>
2025-03-08 23:51:20 +0900

    [feat] pccp4

---
+++ src/main/java/algorithm/programmers/pccp/Sol4.java
@@ -0,0 +1,22 @@
+package algorithm.programmers.pccp;
+
+import java.util.PriorityQueue;
+
+public class Sol4 {
+
+    public long[] solution(int[][] program) {
+        long[] answer = {};
+
+        PriorityQueue pq = new PriorityQueue();
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Sol4 s4 = new Sol4();
+        int[][] sample = {{2, 0, 10}, {1, 5, 5}, {3, 5, 3}, {3, 12, 2}};
+        s4.solution(sample);
+    }
+}
