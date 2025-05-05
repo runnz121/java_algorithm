9c06a26872358faff2df8a3349a5ee078ddde0f2
runnz121 <runnz121@gmail.com>
2025-05-05 20:06:07 +0900

    [feat] _1516 topologiest

---
+++ src/main/java/algorithm/baekjoon/_1516_dp.java
@@ -0,0 +1,17 @@
+package algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _1516_dp {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Integer n = Integer.parseInt(st.nextToken());
+
+
+    }
+}
